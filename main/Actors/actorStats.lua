23d7a893f3f50c2fd2499e1a94921222d5aef936
Kklear <kingklear@gmail.com>
2019-06-21 17:49:06 +0200

    Basic actor script done. Now adding houses to the surface.

---
+++ main/Actors/actorStats.lua
@@ -0,0 +1,14 @@
+local M = {
+	knight = {
+		health = 10,
+		attack = 3,
+		attack_speed = 1
+	},
+	spider = {
+		health = 6,
+		attack = 3,
+		attack_speed = 0.6
+	}
+}
+
+return M
---
+++ main/world/world.go
@@ -0,0 +1,229 @@
+components {
+  id: "main"
+  component: "/main/world/world.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "cave1"
+  type: "sprite"
+  data: "tile_set: \"/main/world/world.atlas\"\n"
+  "default_animation: \"cave\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 320.0
+    y: 800.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "cave2"
+  type: "sprite"
+  data: "tile_set: \"/main/world/world.atlas\"\n"
+  "default_animation: \"cave\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 320.0
+    y: 700.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "cave3"
+  type: "sprite"
+  data: "tile_set: \"/main/world/world.atlas\"\n"
+  "default_animation: \"cave\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 320.0
+    y: 600.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "cave4"
+  type: "sprite"
+  data: "tile_set: \"/main/world/world.atlas\"\n"
+  "default_animation: \"cave\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 320.0
+    y: 500.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "cave5"
+  type: "sprite"
+  data: "tile_set: \"/main/world/world.atlas\"\n"
+  "default_animation: \"cave\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 320.0
+    y: 400.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "cave6"
+  type: "sprite"
+  data: "tile_set: \"/main/world/world.atlas\"\n"
+  "default_animation: \"cave\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 320.0
+    y: 300.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "cave7"
+  type: "sprite"
+  data: "tile_set: \"/main/world/world.atlas\"\n"
+  "default_animation: \"cave\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 320.0
+    y: 200.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "surface"
+  type: "sprite"
+  data: "tile_set: \"/main/world/world.atlas\"\n"
+  "default_animation: \"surface\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 320.0
+    y: 900.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "spawnKnight"
+  type: "factory"
+  data: "prototype: \"/main/Actors/knight.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "spawnSpider"
+  type: "factory"
+  data: "prototype: \"/main/Actors/spider.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "spawnHouse"
+  type: "factory"
+  data: "prototype: \"/main/world/house.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
