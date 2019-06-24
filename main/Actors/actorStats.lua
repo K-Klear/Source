05181cdb26b7de4a756d84e013f7a24e7107498e
Klear <kingklear@gmail.com>
2019-06-24 11:32:34 +0200

    Added goblin graphics

--- main/Actors/actorStats.lua
+++ main/Actors/actorStats.lua
@@ -8,14 +8,21 @@ local M = {
 		health = 6,
 		attack = 3,
 		attack_speed = 0.6,
-		channelTime = 2,
+		channelTime = 0.5,
 		channelCost = 10
 	},
 	peasant = {
 		health = 4,
 		attack = 1,
 		attack_speed = 2
-	}
+	},
+	goblin = {
+		health = 8,
+		attack = 4,
+		attack_speed = 0.8,
+		channelTime = 1,
+		channelCost = 15
+	},
 }
 
 return M
